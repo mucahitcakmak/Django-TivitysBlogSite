56b54aac16991bffce36324286441dd6cf24fefb
Mücahit Çakmak <mucoox18@gmail.com>
2023-03-04 23:56:50 +0300

    Developer-friendly changes made-v1.1

--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include("home.urls")),
+    # path('', include("home.urls")),
     path('user/', include("user.urls")),
     path('admin/', admin.site.urls),
 ]
